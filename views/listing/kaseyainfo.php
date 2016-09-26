5eeb23d55f883f6605b4aee5fae3021d00af4eac
Mikael <mikael@dicom.se>
2016-09-26 15:48:39 +0200

    Update

--- views/listing/kaseyainfo.php
+++ views/listing/kaseyainfo.php
@@ -10,73 +10,9 @@ new kaseyainfo_model;
 
   <div class="row">
     <div class="col-lg-12">
-    <script type="text/javascript">
     
-    $(document).on('appUpdate', function(e){
-
-        var oTable = $('.table').DataTable();
-        oTable.ajax.reload();
-        return;
-
-    });
-
-
-    $(document).on('appReady', function(e, lang) {
-
-        // Get modifiers from data attribute
-        var mySort = [], // Initial sort
-            hideThese = [], // Hidden columns
-            col = 0, // Column counter
-            columnDefs = [{ visible: false, targets: hideThese }]; //Column Definitions
-
-        $('.table th').map(function(){
-
-            columnDefs.push({name: $(this).data('colname'), targets: col});
-
-            if($(this).data('sort'))
-            {
-              mySort.push([col, $(this).data('sort')])
-            }
-
-            if($(this).data('hide'))
-            {
-              hideThese.push(col);
-            }
-
-            col++
-        });
-
-          oTable = $('.table').dataTable( {
-              ajax: {
-                  url: "<?php echo url('datatables/data'); ?>",
-                  type: "POST"
-              },
-              dom: mr.dt.buttonDom,
-              buttons: mr.dt.buttons,
-              order: mySort,
-              columnDefs: columnDefs,
-              createdRow: function( nRow, aData, iDataIndex ) {
-                // Update name in first column to link
-                var name=$('td:eq(0)', nRow).html();
-                if(name == ''){name = "No Name"};
-                var sn=$('td:eq(1)', nRow).html();
-                var link = get_client_detail_link(name, sn, '<?=url()?>/', '#tab_summary');
-                $('td:eq(0)', nRow).html(link);
-
-                
-            }
-          } );
-
-          // Use hash as searchquery
-          if(window.location.hash.substring(1))
-          {
-          oTable.fnFilter( decodeURIComponent(window.location.hash.substring(1)) );
-          }
-
-      } );
-    </script>
-
-      <h3>Kaseya  <span id="total-count" class='label label-primary'>…</span></h3>
+    
+    <h3>Kaseya  <span id="total-count" class='label label-primary'>…</span></h3>
 
       <table class="table table-striped table-condensed table-bordered">
         <thead>
@@ -96,5 +32,48 @@ new kaseyainfo_model;
     </div> <!-- /span 12 -->
   </div> <!-- /row -->
 </div>  <!-- /container -->
+    
+    
+    
+    
+  
+<script type="text/javascript">
+
+	$(document).on('appUpdate', function(e){
+
+		var oTable = $('.table').DataTable();
+		oTable.ajax.reload();
+		return;
+
+	});
+
+	$(document).on('appReady', function(e, lang) {
+		// Get column names from data attribute
+		var columnDefs = [],
+            col = 0; // Column counter
+		$('.table th').map(function(){
+              columnDefs.push({name: $(this).data('colname'), targets: col});
+              col++;
+		});
+	    oTable = $('.table').dataTable( {
+	        columnDefs: columnDefs,
+	        ajax: {
+                url: "<?php echo url('datatables/data'); ?>",
+                type: "POST"
+            },
+            dom: mr.dt.buttonDom,
+            buttons: mr.dt.buttons,
+	        createdRow: function( nRow, aData, iDataIndex ) {
+	        	// Update name in first column to link
+	        	var name=$('td:eq(0)', nRow).html();
+	        	if(name == ''){name = "No Name"};
+	        	var sn=$('td:eq(1)', nRow).html();
+	        	var link = mr.getClientDetailLink(name, sn);
+	        	$('td:eq(0)', nRow).html(link);
+
+	        }
+	    });
+	});
+</script>
 
 <?php $this->view('partials/foot'); ?>
